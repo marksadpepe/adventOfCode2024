976e0a9201c16acef4deb1a834b420968fc578d3
Mykhailo Voznesenskyi <mykhailo.voznesenskyi@amedia.ua>
2025-02-16 01:50:55 +0200

    feat: added solution for the 1st day

---
+++ days/1/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func abs(x int) int {
+  if x < 0 {
+    return -x
+  }
+
+  return x
+}
+
+func sortSlice(slice []int) {
+	for i := 0; i < len(slice); i++ {
+		for j := 0; j < len(slice)-1-i; j++ {
+			if slice[j] > slice[j+1] {
+        slice[j], slice[j+1] = slice[j+1], slice[j]
+			}
+		}
+	}
+}
+
+func main() {
+	file, err := os.Open("input")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	var leftSlice []int
+	var rightSlice []int
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		slice := strings.Split(line, " ")
+
+		leftInt, _ := strconv.Atoi(slice[0])
+		rightInt, _ := strconv.Atoi(slice[1])
+
+		leftSlice = append(leftSlice, leftInt)
+		rightSlice = append(rightSlice, rightInt)
+	}
+
+  sortSlice(leftSlice)
+  sortSlice(rightSlice)
+
+  var sum int
+
+  for i := 0; i < len(leftSlice); i++ {
+    distance := abs(leftSlice[i] - rightSlice[i])
+    sum += distance
+  }
+
+  fmt.Println(sum)
+}
